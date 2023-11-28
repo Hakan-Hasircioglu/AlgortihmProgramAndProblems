a9bb3504dc7119faf5b79b92dfca48191b17fe29
Hakan Hasircioglu <hasircioglu.hakan02@gmail.com>
2023-11-28 21:34:52 +0300

    programed

---
+++ PatternPrograms/Pattern6_/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern6_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 25; i++)
+            {
+                // to make it look neater
+                if (i < 10)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write(" " + i + " ");
+                
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}

f7df28ede781149cbffa27cef839d6de30ea75e9
Hakan Hasircioglu <hasircioglu.hakan02@gmail.com>
2023-12-04 00:02:57 +0300

    Programmed

---
+++ PatternPrograms/Pattern22_/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern22_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            for (int i = 0; i <25; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    num++;
+                    Console.WriteLine();
+                }
+                if (num % 2 == 0) 
+                {
+                    Console.Write("{0,2:D}" , 0);
+                }
+                else
+                {
+                    Console.Write("{0,2:D}", 1);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}

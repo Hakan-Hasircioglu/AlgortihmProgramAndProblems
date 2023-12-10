3be8f8d016c92b401e6acd69253aac924460c2bc
Hakan Hasircioglu <hasircioglu.hakan02@gmail.com>
2023-12-10 22:24:08 +0300

    pushed

---
+++ PatternProgramsForBeginners/Pattern7_/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern7_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 50; i+=2)
+            {
+<<<<<<< Updated upstream
+                Console.Write("{0,3:D}", i );
+=======
+                Console.Write("{0,3:D}", i);
+>>>>>>> Stashed changes
+                
+                if (i % 10 == 9)
+                {
+                    Console.WriteLine();
+                }
+            }
+            
+        }
+    }
+}

527fe2c18b8ae72a87cdeca32591826ca4cba572
Hakan Hasircioglu <hasircioglu.hakan02@gmail.com>
2023-12-10 23:04:09 +0300

    Programs

---
+++ PatternProgramsForElemantary/Pattern35_/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern35_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("{0,2:D}" , i);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

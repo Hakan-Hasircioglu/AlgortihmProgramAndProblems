695a205844865176c9045d588d666ce10c31d73f
Hakan Hasircioglu <hasircioglu.hakan02@gmail.com>
2023-12-17 19:22:28 +0300

    Programmed

---
+++ PatternProgramsForElemantary/Pattern47_/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern47_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int after;
+            for (int i = 1; i <= 5; i++)
+            {
+                after = i;
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("{0,3:D}" , after);
+                    after += i;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

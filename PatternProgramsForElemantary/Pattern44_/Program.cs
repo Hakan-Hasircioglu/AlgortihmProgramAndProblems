a2c54c9689ddc37a613c3989b264ffc48f48a96b
Hakan Hasircioglu <hasircioglu.hakan02@gmail.com>
2023-12-17 19:11:28 +0300

    prgrmmd

---
+++ PatternProgramsForElemantary/Pattern44_/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern44_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            int add = 1, iBefore;
+            for (int i = 1; i <= 10; i += add)
+            {
+                iBefore = i;
+                for (int j = 1; j <= add; j++)
+                {
+                    Console.Write("{0,3:D}" , i);
+                    i--;
+                }
+                i = iBefore;
+                add++;
+                Console.WriteLine();
+            }
+        }
+    }
+}

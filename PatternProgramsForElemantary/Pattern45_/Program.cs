2418fa861145edeef93f160a545e7b40d0538e5b
Hakan Hasircioglu <hasircioglu.hakan02@gmail.com>
2023-12-17 19:09:19 +0300

    Programmed

---
+++ PatternProgramsForElemantary/Pattern45_/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern45_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 1;
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("{0,3:D}", sum);
+                    sum += 2;
+                }
+                Console.WriteLine();
+            }
+        }
+    }    
+}

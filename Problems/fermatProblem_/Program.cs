7b8ab852d4360dd0a28c0d06de8ad5500a6f1b3d
Hakan Hasircioglu <hasircioglu.hakan02@gmail.com>
2023-12-25 22:10:07 +0300

    update

--- Problems/fermatProblem_/Program.cs
+++ Problems/fermatProblem_/Program.cs
@@ -4,7 +4,6 @@ namespace fermatProblem_
 {
     internal class Program
     {
-
         static void Main(string[] args)
         {        
             // doksandokuza kadar olan sayilari topluyoruz
@@ -15,11 +14,12 @@ namespace fermatProblem_
             }
             // uc haneden baslamak icin fermat oncesi olarak toplam kullaniyoruz
             int fermatOncesi = 0, fermatOncesiToplam = toplam;
-            int ekle = 1;
-            int fermatSonrasi = 0;
+            int ekle = 0;
+            int fermatSonrasi;
             // sonuc 3 haneli oldugu icin sinirimiz 100-999
-            for (int fermat = 100; fermat <= 999; fermat++)
+            for (int fermat = 100; fermat <= 15550; fermat++)
             {
+                ekle = fermat + 1;
                 //Oncesi
                 //yuzden sonra da her sayimiz ilerlediginde oncesini hesapliyoruz
                 fermatOncesiToplam += fermat;
@@ -31,20 +31,25 @@ namespace fermatProblem_
                  *  fermat sonrasi fermat oncesinden kucuk veya esit oldugu surece sonrasini hesapliyoruz eger sonrasi,
                  * fermat oncesini gecerse sayimiz tutmadigi icin o sayiyi geciyoruz
                  */
-                for (; fermatSonrasi <= fermatOncesi; fermatSonrasi += fermat + ekle)
+                while (fermatSonrasi <= fermatOncesi)
                 {
+                    fermatSonrasi += ekle;
+                    
+                       
+                    
                     /*  dongu icerisinde fermat sayisindan sonraki sayilari eklemek icin 'ekle' isminde degisken atiyoruz
                      * bu degiskeni her 1 arttirip fermatla beraber topladigimizda fermatSonrasina erisiyoruz ve sonraki
                      * fermatSonrasini oncekilerle kumulatif sekilde atiyoruz */
                     // fermatOncesi ve fermatSonrasi birbirine esit ise kapi numarasini ve esit geldigi degerleri yazdiriyoruz
-                    if (fermatSonrasi == fermatOncesi)
+                    if (fermatOncesi == fermatSonrasi)
                     {
-                        Console.WriteLine("********* " + "Fermat Kapi No: " + fermat + "    *********"); //  963
-                        Console.WriteLine("********* " + "Fermat Oncesi: " + fermatOncesi + "  *********"); // 463203
-                        Console.WriteLine("********* " + "Fermat Sonrasi: " + fermatSonrasi + " *********"); // 463203
+                        Console.WriteLine("fermat kapi no: {2} fermat oncesi: {1}, fermat sonrasi: {0}", fermatSonrasi, fermatOncesi, fermat);
+                        Console.WriteLine(new string('*' , 50));
+                        break;
                     }
                     ekle++;
                 }
+                fermatSonrasi = 0;
             }
         }
     }

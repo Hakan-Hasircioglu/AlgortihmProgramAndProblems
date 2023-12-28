12252cea11d44e90f972001bc75ccc72d222a83d
Hakan Hasircioglu <hasircioglu.hakan02@gmail.com>
2023-12-29 02:26:44 +0300

    Alt. Pattern59_

---
+++ PatternProgramsForElemantary/Pattern60_/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern60_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j <=i; j++)
+                {
+                    a = i % 2;
+                    Console.Write($"{a,2}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

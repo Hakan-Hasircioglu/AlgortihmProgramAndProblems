62e9df2ff472625f3fbd081f64a95440e0c058ef
Hakan Hasircioglu <hasircioglu.hakan02@gmail.com>
2024-01-02 23:48:00 +0300

    Programm

---
+++ PatternProgramsForElemantary/Pattern64_/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern64_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 0;
+            for (int i = 1; i <= 5; i++)
+            {
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{(j + k) % 2,2}");
+                }
+                Console.WriteLine();
+                k++;
+            }
+        }
+    }
+}

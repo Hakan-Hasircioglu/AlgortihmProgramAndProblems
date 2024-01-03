b0c8d45ca94d2dc01be572d52fb3d164c301b2f8
Hakan Hasircioglu <hasircioglu.hakan02@gmail.com>
2024-01-03 22:58:56 +0300

    Programm

---
+++ PatternProgramsForElemantary/Pattern69_/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern69_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Char k;
+            for (char i = 'A'; i <= 'E'; i++)
+            {
+                k = i;
+                for (char j = 'A'; j <=i; j++)
+                {
+                    Console.Write($"{k,2}");
+                    k--;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

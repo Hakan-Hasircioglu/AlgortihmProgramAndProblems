f3c9155acf817630a988d7276bef1eb6c51e200a
Hakan Hasircioglu <hasircioglu.hakan02@gmail.com>
2024-01-06 23:27:34 +0300

    Programm

---
+++ PatternProgramsSpecial/PatternTriangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternTriangle
+{
+    internal class Program
+    {static void Main(string[] args)
+        {
+            int number = 5; //Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = number - 1; j >= i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = i; k >= 1; k--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                for (int l = 0; l <= 1; l++)
+                {
+
+                }
+            }
+        }
+    }
+}

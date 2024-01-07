caf8653b23a7b4500bf59322248e082c21eada02
Hakan Hasircioglu <hasircioglu.hakan02@gmail.com>
2024-01-07 04:49:09 +0300

    Programm

---
+++ PatternProgramsSpecial/PatternTriangle_/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternTriangle_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int input_ = Convert.ToInt32(Console.ReadLine());
+                if (input_ == 0 || input_ < 1 )
+                    Console.WriteLine
+                        (
+                        $"Try the available numbers" +
+                        $"\tAvailabe numbers > 0 "
+                        );
+                for (int i = 1; i <= input_; i++)
+                {
+                    for (int j = input_ - 1; j >= i; j--)
+                        Console.Write(" ");
+                    Console.Write(" /");
+                    if (i == input_)
+                        Console.Write(new string('_', (input_ * 2)-2));
+                    for (int h = 1; h < i; h++)
+                    {
+                        if (i >= input_ )
+                            continue;
+                        else
+                            Console.Write("  ");
+                    }
+                    Console.Write("\\"); Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ PatternProgramsSpecial/PatterrnRectangle_/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatterrnRectangle_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int input_ = Convert.ToInt32(Console.ReadLine());
+                if (input_ == 0)
+                    continue;
+                Console.WriteLine(new string('_', (input_ * 2) + 2));
+                for (int i = 1; i <= input_; i++)
+                {
+                    Console.Write('|');
+                    for (int j = 1; j <= (input_ * 2); j++)
+                    {
+                        if (i == input_)
+                        {
+                            Console.Write(new string('_', input_ * 2));
+                            break;
+                        }
+                        else
+                            Console.Write(" ");
+                    }
+                    Console.Write('|');
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}

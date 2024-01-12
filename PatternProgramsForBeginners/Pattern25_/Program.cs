64a9d01a827e7bf08e5e280aca3efb499130fef0
Hakan Hasırcıoğlu <119371782+Hakan-Hasircioglu@users.noreply.github.com>
2024-01-12 23:10:49 +0300

    Update Program.cs

--- PatternProgramsForBeginners/Pattern25_/Program.cs
+++ PatternProgramsForBeginners/Pattern25_/Program.cs
@@ -13,9 +13,8 @@ namespace Pattern25_
             for (int i = 1; i <= 5; i++)
             {
                 for (int j = 1; j <= 5; j++)
-                {
                     Console.Write((j % 2) + " ");
-                }
+                
                 Console.WriteLine();
             }
         }

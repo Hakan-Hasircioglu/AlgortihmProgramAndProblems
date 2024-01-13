c0abc40d98b75717e3d98a95e120ac1fbe915796
Hakan Hasircioglu <hasircioglu.hakan02@gmail.com>
2024-01-13 23:57:49 +0300

    Prog

---
+++ PatternProgramsSpecial/PatternMultiplicationTable_/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternMultiplicationTable_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = -1;
+            for (int i = 1; i <=10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.Write($"{i}x{j} = {i * j}| ");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}

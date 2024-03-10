559783b5af52c4da93fd7bd4000b8d42808db97b
Hakan Hasircioglu <hasircioglu.hakan02@gmail.com>
2024-03-11 00:57:16 +0300

    Files

---
+++ PatternProgramsForElemantary/Pattern94_/Pattern94_.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k;
+    k = 65;
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 0; j < i; j++)
+        {
+            printf("%c ", k + j);
+        }
+        printf("\n");
+        k+=i;
+    }
+    
+    return 0;
+}

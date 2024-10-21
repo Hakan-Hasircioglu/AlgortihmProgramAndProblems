3e89faca0c76c9f4b7fb47a622e0a9f8db693f02
Hakan Hasırcıoğlu <119371782+Hakan-Hasircioglu@users.noreply.github.com>
2024-10-21 15:20:04 +0300

    Update ProblemFive.cs

--- Problems/Problem5_/ProblemFive.cs
+++ Problems/Problem5_/ProblemFive.cs
@@ -11,7 +11,7 @@ namespace Problem5_
         static void Main(string[] args)
         {
             /* 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-              What is the smallest positive number that is evenly divisible by all of the numbers from to ?
+              What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
             */
             bool stopIt = false;
             for (int i = 1; ; i++) //infinite loop start
